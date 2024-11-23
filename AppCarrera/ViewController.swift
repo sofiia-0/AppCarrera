81c946fb0adba6c2b8433ae5ca055a2dcb22fac9
sofiia-0 <ana.sofiia02@gmail.com>
2024-11-22 18:41:42 -0800

    Initial Commit

---
+++ AppCarrera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppCarrera
+//
+//  Created by User-UAM on 11/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
